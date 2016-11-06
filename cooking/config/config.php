d8722b080a829e48a7a6315664ff844679deb830
Siujade <siujade@gmail.com>
2016-11-06 16:41:52 +0200

    Update config.php

--- cooking/config/config.php
+++ cooking/config/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_CONNECTION_STRING', 'mysql:host=localhost;dbname=nshadene_cooking');
-define('DB_USER', 'nshadene_admin');
-define('DB_PASS', 'e3Na8v6y+0Zq');
+define('DB_CONNECTION_STRING', 'mysql:host=localhost;dbname=*****');
+define('DB_USER', '******');
+define('DB_PASS', '*****');
 define('DEFAULT_CONTROLLER', 'articles');
 define('DEFAULT_ACTION', 'index');
 define('DEFAULT_LAYOUT', 'default');
